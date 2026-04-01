6f34f1050fc66cf62358e5cd4ad2a4198dfdfefd
Ibrahim Maulid <ibrahimmaulid551@example.com>
2026-04-01 04:40:24 +0300

    delete dublicated frontend folder

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,7 +6,8 @@ import axios from "axios";
  * The baseURL should point to your backend server (e.g., http://localhost:5000/api).
  */
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://quizzapp-1-k5ls.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
